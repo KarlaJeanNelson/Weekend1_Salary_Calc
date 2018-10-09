4feb6b2d6bc71f24d0f5ee1522888b623879f44c
KarlaJeanNelson <karla.jean.nelson@gmail.com>
2018-10-09 14:15:11 -0500

    cleaned up some tech debt

--- client.js
+++ client.js
@@ -2,12 +2,15 @@
 
 console.log('js');
 $(ready);
+// $(document).ready( function () {
+// 	$('#myTable').DataTable();
+// } );
 
 let employees = [];
 const threshhold = 20000;
 
 class Employee {
-	constructor(firstname, lastname, title, id, salary) {
+	functionructor(firstname, lastname, title, id, salary) {
 		this.employeeid = id;
 		this.firstname = firstname;
 		this.lastname = lastname;
@@ -19,30 +22,42 @@ class Employee {
 function ready() {
 	console.log('jq');
 	$("form").on("submit", submitForm);
+	$("table").on("click", "button", editOrDel)
 }
 
 function submitForm(e) {
 	e.preventDefault()
-	// console.log('in submitForm');
+	// console.log('in submitForm', $(this));
 	let ar = $(this).serializeArray();
-	addToList(employeeArray(ar));
+	// console.log('ar', ar);
+	let newPerson = createEmployee(ar);
+	// console.log(newPerson);
+	addToList(newPerson);
 	updateDOM();
+	resetForm();
 }
 
-const employeeArray = array => {
-	console.log('in employeeArray');
+function resetForm() {
+	$("form input").val('');
+	$("#employeeid").focus();
+	return;
+}
+
+function createEmployee(array) {	
+	console.log('in employeeArray', array);
 	let newEmployee = new Employee()
-	array.forEach(value => {
-		Object.defineProperty(newEmployee, value.name, {
-			value: value.value
-		});
+	console.log('newEmployee', newEmployee);
+	array.forEach(function(value) {
+		console.log(value);
+		newEmployee[value.name] = value.value		
 	})
+	console.log(newEmployee.employeeid);
 	newEmployee.employeeid = cleanId(newEmployee.employeeid)
 	newEmployee.salary = parseFloat(newEmployee.salary).toFixed(2);
 	return newEmployee;
 }
 
-const cleanId = id => {
+function cleanId(id) {
 	// remove whitespace
 	let cleanId = id.replace(/\s/g, '');
 	// remove non-alphanumeric from beginning and end
@@ -50,59 +65,66 @@ const cleanId = id => {
 	return cleanId;
 }
 
-const checkId = id => {
-	const match = employees.find(employee => employee.employeeid === id);
-	console.log(match);
-	if (match === undefined) {
-		return true;
-	} else {
-		// TODO: allow user to update existing employee.
-		alert('Employee with this id already exists.')
-		return false;
+function checkId(id) {
+	let match = findId(id).employeeObj;
+	console.log('in checkId, match', match);
+	if (match !== undefined) {
+		alert('Employee with this id already exists.');
+	}
+	return match === undefined;
+}
+
+function findId(id) {
+	return {
+		employeeObj: employees.find(employee => employee.employeeid === id),
+		index: employees.findIndex(employee => employee.employeeid === id)
 	}
 }
 
-const checkSalary = employee => {
+function checkSalary(employee) {
 	console.log(employee.salary);
 	if (isNaN(Number(employee.salary)) || Number(employee.salary) < 0) {
 		alert('Salary must be a non-negative number.');
-		return false;
-	} else {
-		return true;
 	}
+	// console.log(!isNaN(Number(employee.salary)) && Number(employee.salary) > 0);
+	return !isNaN(Number(employee.salary)) && Number(employee.salary) >= 0
 }
 
-const addToList = employee => {
+function addToList(employee) {
 	if (checkSalary(employee) && checkId(employee.employeeid)) {
 		employees.push(employee);
 	}
 	return employees;
 }
 
-const updateDOM = () => {
+function updateDOM() {
 	// console.log('in updateDOM');
 	// console.log('employees', employees);
+	// TODO(DONE): split out button addition
 	$("tbody").empty()	
 	for (let i of employees) {
-		$("tbody").append("<tr></tr>");
+		console.log(i);		
+		$("tbody").append("<tr></tr>").data('employeeId', i.employeeid);
 		$("tr:last").append(`<td>${i.employeeid}</td>`)
 		.append(`<td>${i.firstname}</td>`)
 		.append(`<td>${i.lastname}</td>`)
 		.append(`<td>${i.jobtitle}</td>`)
 		.append(`<td class="has-text-right">${i.salary}</td>`)
 		.append(`<td class="has-text-right"><button class="button is-danger is-small">Delete</td>`)
-
-		$("button:last")
-		.attr("id", i.employeeid)
-		.on("click", removeRow);
+		addButtons(i);
 	}
 	toggleClass(updateTotal(), $("tfoot"), "red");
-	$("form input").val('');
-	$("#employeeid").focus();
 	return;
 }
 
-const updateTotal = () => {
+function addButtons(emp) {
+	$("button:last").data('employeeId', emp.employeeid)
+
+	$("td:last").append(`<button class="button is-link is-small">Edit</td>`);
+	$("button:last").data('employeeId', emp.employeeid)
+}
+
+function updateTotal() {
 	const total = employees.reduce((a, b) => {
 		return a + Number(b.salary)
 	}, 0);
@@ -111,19 +133,61 @@ const updateTotal = () => {
 	return parseFloat($("#monthlytotal").html()) > threshhold;
 }
 
-const toggleClass = (bool, element, className) => {
-	bool ? element.addClass(className) : element.removeClass(className)
+function toggleClass(bool, element, className) {
+	bool ? element.addClass(className) : element.removeClass(className);
 	return;
 }
 
-const removeRow = e => {
-	console.log(e.target.id);
+function editOrDel(e) {
+	console.log('in editOrDel');
+	console.log($(this).html());
+	if ($(this).html() === 'Delete') {
+		removeRow($(this).data());
+	} else {
+		editRow($(this).data());
+	}
+	return;
+}
+
+function removeRow(data) {
+	console.log('in removeRow', data.employeeId);
+	
 	// TODO: alert to confirm deletion.
-	const index = employees.findIndex(employee => employee.employeeid === e.target.id)
+	let index = findId(data.employeeId).index;
 	console.log(index);
 	employees.splice(index, 1);
-	return updateDOM();	
+	updateDOM();
+	return;	
 }
+
+function editRow(id) {
+	// do stuff
+	$('table').find(`[html=${id.employeeid}]`).addClass("red");
+}
+
+// function editRow1(e) {
+// 	let id = e.target.id;
+// 	id = id.substring(0, id.length - 5);
+// 	// console.log('in editRow', id);
+// 	let match = findId(id).employeeObj;
+// 	console.log(match);
+	
+// 	let formInputs = $("form input")
+// 	for (let input of formInputs) {
+// 		console.log('input.id', input.id);
+// 		$("form").find(`#${input.id}`).val()
+// 		// input.attr("value", "hi")
+// 	}
+// }
+
+// function editRow(e) {
+// 	let myTable = $("#myTable");
+// $('#myTable').on( 'click', 'tbody tr', function () {
+// 	myTable.row( this ).edit();
+// } );
+// }
+
+
 
 // The application should have an input form that collects _employee first name, last name, ID number, job title, annual salary_.
 
